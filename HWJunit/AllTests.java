05d81169372d96ea69618fc279bbb307b980d282
Partha Saradhi Tatikondad <tpardhu561@gmail.com>
2017-06-26 22:16:07 -0400

    Re-created reports folder

--- HWJunit/AllTests.java
+++ HWJunit/AllTests.java
@@ -14,10 +14,9 @@ public class AllTests
 	//such as Eclipse.  However, if you are using Java on the command line or
 	//with a simpler IDE like JGrasp or jCreator, the following main() and suite()
 	//might be helpful.
-
+	
     // Execution begins at main().  In this test class, we will execute
     // a text test runner that will tell you if any of your tests fail.
-	
     public static void main (String[] args) 
     {
        junit.textui.TestRunner.run (suite());
